42dbffce2f54631634416ee78f132f6f55e89a28
ricardobizerra <ricardo.bizerra19@gmail.com>
2023-12-18 23:59:06 -0300

    feat: implement epsilon-greedy policy

--- client.py
+++ client.py
@@ -2,6 +2,7 @@
 
 import connection as con
 import random as rd
+import numpy as np
 
 cn = con.connect(2037)
 
@@ -9,14 +10,17 @@ cn = con.connect(2037)
 #linhas n até n + 3 são plataforma n norte, leste, sul e oeste respectivamente 
 
 #Função para recuperar o progresso da q_table
-def recover_table(table):
+def recover_table():
+    table = []
     with open("resultado.txt", "r") as file:
         text = file.readlines()
         
     for n in range(96):
         line = text[n].split(" ")
-        for m in range(3):
-            table[n][m] = float(line[m])
+        pos = [float(val) for val in line]
+        table.append(pos)
+        
+    return table
 
 #função para iniciar a q table com 96 espaços para os estados, cada um com 3 comandos possíveis
 def init_table(table):
@@ -38,37 +42,45 @@ def reset_table():
         
 #função responsável por atualizar a tabela, sendo x e y as cordenadas na matriz
 def q_update(q_table, x, y, reward, alfa, gama, max):
-    q_table[x][y] = (1 - alfa) * q_table[x][y] + (alfa * (reward + (gama * max)))
+    if x < len(q_table) and y < len(q_table[x]): q_table[x][y] = (1 - alfa) * q_table[x][y] + (alfa * (reward + (gama * max)))
 
-q_table = []
-init_table(q_table)        
+q_table = recover_table()
 
 alfa = 0.25 # Taxa de aprendizagem
 gama = 0.50 # Taxa de desconto
+epsilon = 0.1 # Epsilon value for epsilon-greedy policy
 
 act = ['jump','left','right']
 
-recover_table(q_table)
-repeat = 0
-while repeat < 1000:
+while True:
+    print('==============================')
     estado, recompensa = con.get_state_reward(cn, rd.choice(act))
     print(f'Estado: {estado} | Recompensa: {recompensa}')
 
     plataforma, direcao = int(estado[:7],2), int(estado[7:],2)
     print(f'Plataforma: {plataforma} | Direção: {direcao}')
     
-    repeat += 1
+    # Epsilon-greedy policy
+    if rd.random() < epsilon:
+        action = rd.choice(act)  # Exploration
+    else:
+        action = act[np.argmax(q_table[plataforma])]  # Exploitation
     
+    estado, recompensa = con.get_state_reward(cn, action)
+    plataforma_nova, direcao_nova = int(estado[:7],2), int(estado[7:],2)
     
-#salva os resultados atuais    
-with open("resultado.txt", "w") as file:
-    text = ""
-    
-    for n in range(96):
-        for m in range(3):
-            if m < 2:
-                text = f"{text}{q_table[n][m]} "
-            else:
-                text = f"{text}{q_table[n][m]}\n"
-    
-    file.write(text)
+    max_q_value = max(q_table[plataforma_nova])
+    q_update(q_table, plataforma, direcao, recompensa, alfa, gama, max_q_value)   
+        
+    #salva os resultados atuais    
+    with open("resultado.txt", "w") as file:
+        text = ""
+        
+        for n in range(96):
+            for m in range(3):
+                if m < 2:
+                    text = f"{text}{q_table[n][m]} "
+                else:
+                    text = f"{text}{q_table[n][m]}\n"
+        
+        file.write(text)
